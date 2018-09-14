8ab1774175cb5bcb34014e409a16936f24a882e4
Vincent Chi <song374561@chivincent.net>
2018-09-14 10:12:36 +0800

    Update application namespace.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Providers;
+namespace NFUWorkshop\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -12,7 +12,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
+        'NFUWorkshop\Model' => 'NFUWorkshop\Policies\ModelPolicy',
     ];
 
     /**
