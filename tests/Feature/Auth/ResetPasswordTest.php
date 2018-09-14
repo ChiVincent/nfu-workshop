148d60e8f19680ea4dc8f6aaef358f887683bb91
Vincent Chi <song374561@chivincent.net>
2018-09-14 11:08:41 +0800

    Create reset password test case.

---
+++ tests/Feature/Auth/ResetPasswordTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Carbon\Carbon;
+use NFUWorkshop\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ResetPasswordTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testResetPassword()
+    {
+        $user = factory(User::class)->create();
+        \DB::insert('INSERT INTO password_resets (`email`, `token`, `created_at`) VALUES (?, ?, ?)', [
+            $user->email, bcrypt($token = str_random(20)), Carbon::now()
+        ]);
+
+        $response = $this->post(route('password.update'), [
+            'token' => $token,
+            'email' => $user->email,
+            'password' => 'new_password',
+            'password_confirmation' => 'new_password',
+        ]);
+
+        $response->assertRedirect(route('home'));
+        $this->assertTrue(\Hash::check('new_password', User::find($user->id)->password));
+    }
+}
